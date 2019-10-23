048750759bb51b5f58bd21ffae509ca07bed9248
Michal Trochimiuk <michal.trochimiuk@boldare.com>
2019-10-23 13:36:35 +0200

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-aws configure set aws_access_key_id $PLUGIN_ACCESS_KEY
-aws configure set aws_secret_access_key $PLUGIN_SECRET_ACCESS_KEY
-aws configure set default.region $PLUGIN_REGION
-
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY}
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+aws configure set default.region ${AWS_REGION}
+$(aws ecr get-login --no-include-email)
